9b7eb6b36c1b4fed9234c9026090f33a4a07f532
Gabriel Tessaro Favareto <gabrieltessarofavareto@gmail.com>
2024-06-01 15:52:13 -0300

    Configurações adicionais para funcionar no docker

--- src/test/java/com/pagamento/PagamentomcApplicationTests.java
+++ src/test/java/com/pagamento/PagamentomcApplicationTests.java
@@ -3,7 +3,7 @@ package com.pagamento;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PagamentomcApplicationTests.class)
 class PagamentomcApplicationTests {
 
 	@Test
