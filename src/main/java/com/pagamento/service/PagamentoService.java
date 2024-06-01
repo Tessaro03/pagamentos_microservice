da7b4b2d55e4c67d324b69aca0800e826ce93fbb
Gabriel Tessaro Favareto <gabrieltessarofavareto@gmail.com>
2024-06-01 15:51:49 -0300

    alteração de envio para exchange

--- src/main/java/com/pagamento/service/PagamentoService.java
+++ src/main/java/com/pagamento/service/PagamentoService.java
@@ -43,7 +43,7 @@ public class PagamentoService {
         var pagamento = repository.pagamentoPorIdPedido(idPedido);
         pagamento.get().setStatus(Status.CONFIRMADO);
         repository.save(pagamento.get());
-        rabbitTemplate.convertAndSend("pagamento.concluido", pagamento.get().getPedidoId());
+        rabbitTemplate.convertAndSend("pagamento.ex","", pagamento.get().getPedidoId());
     }
 
 
