bf2991396bfdd06453c46a025006196c0b41f309
Gabriel Tessaro Favareto <gabrieltessarofavareto@gmail.com>
2024-07-22 17:59:55 -0300

    Adicionado SpringDoc

--- src/main/java/com/pagamento/controller/PagamentoController.java
+++ src/main/java/com/pagamento/controller/PagamentoController.java
@@ -6,15 +6,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pagamento.dtos.PagamentoInputDTO;
 import com.pagamento.service.PagamentoService;
 
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/pagamentos")
@@ -24,23 +21,21 @@ public class PagamentoController {
     private PagamentoService service;
 
     @GetMapping
+    @Operation(summary = "Ver Pagamentos", description = "Retorna os pagamentos criado")
     public ResponseEntity ver(){
         return ResponseEntity.ok().body(service.verPagamentos());
     }
 
-    @PostMapping
-    public ResponseEntity criar(@RequestBody @Valid PagamentoInputDTO dto){
-        service.criarPagamento(dto);
-        return ResponseEntity.ok().build();
-    }
 
     @PatchMapping("{idPedido}/confirmado")
+    @Operation(summary = "Confirma o pagamento", description = "Confirma o pagamento e notifica a API de Pedidos e Avaliação")
     public void confirmarPagamento(@PathVariable Long idPedido){
         service.confirmaPagamento(idPedido);
            
     }
 
     @DeleteMapping("{idPedido}/cancelado")
+    @Operation(summary = "Cancela o pagamento", description = "Cancela o pagamento e notifica a API de Pedidos")
     public void cancelarPagamento(@PathVariable Long idPedido){
         service.cancelarPagamento(idPedido);
         
